8ea7a9c43d36935a2be66ed498b8b172abbb7fd0
Romeo <romeolacerdafarias@gmail.com>
2026-06-09 20:38:01 -0300

    finish poc

--- chat/servidor.py
+++ chat/servidor.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 HOST = '127.0.0.1'
-PORT = 55555
+PORT = 55556
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -14,12 +14,13 @@ def broadcast(sala, mensagem):
     for i in salas[sala]:
         if isinstance(mensagem, str):
             mensagem = mensagem.encode()
+
         i.send(mensagem)
 
-def sendMessage(name, sala, client):
+def enviarMensagem(nome, sala, client):
     while True:
         mensagem = client.recv(1024)
-        mensagem = f"{name}: {mensagem.decode()}\n"
+        mensagem = f"{nome}: {mensagem.decode()}\n"
         broadcast(sala, mensagem)
 
 while True:
@@ -27,11 +28,10 @@ while True:
     client.send(b'SALA')
     sala = client.recv(1024).decode()
     nome = client.recv(1024).decode()
-
     if sala not in salas.keys():
         salas[sala] = []
     salas[sala].append(client)
-    print(f'{nome} se conectou na sala {sala}! INFO{addr}')
-    broadcast(sala, f'{nome}: Entrou na sala')
-    thread = threading.Thread(target=sendMessage, args=(nome, sala, client))
+    print(f'{nome} se conectou na sala {sala}! INFO {addr}')
+    broadcast(sala, f'{nome}: Entrou na sala!\n')
+    thread = threading.Thread(target=enviarMensagem, args=(nome, sala, client))
     thread.start()
